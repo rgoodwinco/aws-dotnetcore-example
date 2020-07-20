e1bb3f4a319f8f3bb7e5fc55e38861577b7e5290
Goodwin <rrobg@amazon.com>
2020-07-20 11:23:56 -0600

    CodePipeline

---
+++ before-install.bat
@@ -0,0 +1,5 @@
+REM Install Internet Information Server (IIS).
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+
+c:\work\dotnet-hosting-3.1.5-win.exe /install /quiet /log 
